b6fe086214e3703434f648427d45a1f8d9bae5b5
Jiang Fengming <jiangfengming87@gmail.com>
2021-01-04 18:27:01 +0800

    remove log

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ module.exports = () => ({
     const regex = /:global\(\.(--[\w-]+)/g;
 
     while ((match = regex.exec(style)) !== null) {
-      console.log(match);
       clsArr.push(match[1]);
     }
 
